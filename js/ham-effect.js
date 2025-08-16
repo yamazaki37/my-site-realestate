9494310a6224eee4cd97d87ed877c7bad96385f1
yamazaki37 <uni.yamazaki37@gmail.com>
2025-08-16 10:43:51 +0900

    Add files via upload

---
+++ js/ham-effect.js
@@ -0,0 +1,13 @@
+﻿
+$(function () {
+	$('.trigger').click(function () {
+		$(this).toggleClass('active');
+		$('.triggerBg').fadeToggle();
+		$('nav').toggleClass('open');
+	})
+	$('.triggerBg').click(function () {
+		$(this).fadeOut();
+		$('.trigger').removeClass('active');
+		$('nav').removeClass('open');
+	});
+})
